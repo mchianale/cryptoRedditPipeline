83bf5a99ccb1b7bc894aca9b7e63b05f8935bc0e
Oussama MAHDJOUR <mahdjouroussama@gmail.com>
2025-02-07 12:22:38 +0100

    test health checks

--- schedulerService/main.py
+++ schedulerService/main.py
@@ -3,33 +3,36 @@ import requests
 import logging
 from dotenv import load_dotenv
 import os
-import datetime 
+import datetime
 import schedule
 
 # Configure Logging
-logging.basicConfig(level=logging.INFO, 
-                    format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 # API Configuration
 # Load environment variables from the .env file
 load_dotenv()
 # API endpoints
-HEALTH_ENDPOINT = os.environ["HEALTH_ENDPOINT"]
-SEND_ENDPOINT = os.environ["SEND_ENDPOINT"]
-#elatsic
-ES_ENDPOINT = os.environ["ES_ENDPOINT"]
+REDDIT_API = "http://reddit-api:8000"
+HEALTH_ENDPOINT = f"{REDDIT_API}/health_check"
+SEND_DATA_ENDPOINT = f"{REDDIT_API}/send_data"
+# elatsic
+ES_ENDPOINT = "http://elasticsearch:9200"
 # pieplien hour trigger
 REQUEST_NEW_DATA_HOURS = os.environ["REQUEST_NEW_DATA_HOURS"]
 # pretty logging
-SEP = "-"*30
+SEP = "-" * 30
+
 
 def send_data_to_kafka():
-    """ Calls the FastAPI endpoint to send new data to Kafka """
+    """Calls the FastAPI endpoint to send new data to Kafka"""
     try:
         start_time = time.time()
         logging.info(f"Triggering API to send data to Kafka... {SEP}")
-        date_today = datetime.datetime.today().strftime('%d/%m/%Y')
-        url = f"{SEND_ENDPOINT}?start_date={date_today}&end_date={date_today}"
+        date_today = datetime.datetime.today().strftime("%d/%m/%Y")
+        url = f"{HEALTH_ENDPOINT}?start_date={date_today}&end_date={date_today}"
         logging.info(f"Calling: {url}...")
         response = requests.post(url)
         end_time = time.time()
@@ -37,7 +40,9 @@ def send_data_to_kafka():
         if response.status_code == 200:
             logging.info(f"Response: {response.text}")
         else:
-            logging.error(f"Failed to send data: {response.status_code} {response.text}")
+            logging.error(
+                f"Failed to send data: {response.status_code} {response.text}"
+            )
         logging.info(f"Duration of the process: {duration_minutes:.2f} minutes")
         logging.info(f"{SEP}")
 
@@ -45,53 +50,46 @@ def send_data_to_kafka():
         logging.error(f"Error sending data: {e}")
         logging.info(f"{SEP}")
 
-# schedule 
+
+# schedule
 # Schedule API Call Every Day at REQUEST_NEW_DATA_HOURS hh:mm
 my_schedule = schedule.Scheduler()
 my_schedule.every().day.at(REQUEST_NEW_DATA_HOURS).do(send_data_to_kafka)
 
-if __name__ == "__main__":
-    # check our fast API is running
+
+def check_health(endpoint):
     max_retry = 0
-    while True:
+
+    while max_retry < 30:
         if max_retry == 30:
-            raise f"Impossible to ping {HEALTH_ENDPOINT}"
+            raise f"Impossible to ping {endpoint}"
         try:
-            response = requests.get(HEALTH_ENDPOINT)
+            response = requests.get(endpoint)
             if response.status_code == 200:
-                logging.info(f"Successfuly ping {HEALTH_ENDPOINT}")
-                break
+                logging.info(f"Successfuly ping {endpoint}")
+                return True
             else:
-                logging.info(f"Trying to ping {HEALTH_ENDPOINT}, retry_number={max_retry}")
+                logging.info(f"Trying to ping {endpoint}, retry_number={max_retry}")
                 max_retry += 1
         except Exception as e:
-            logging.info(f"Trying to ping {HEALTH_ENDPOINT}, retry_number={max_retry}, error={str(e)}")
+            logging.info(
+                f"Trying to ping {endpoint}, retry_number={max_retry}, error={str(e)}"
+            )
             max_retry += 1
         time.sleep(1)
 
-    # check elastic search is good to
-    max_retry = 0
-    while True:
-        if max_retry == 30:
-            raise f"Impossible to ping {ES_ENDPOINT}"
-        try:
-            response = requests.get(ES_ENDPOINT)
-            if response.status_code == 200:
-                logging.info(f"Successfuly ping {ES_ENDPOINT}")
-                break
-            else:
-                logging.info(f"Trying to ping {ES_ENDPOINT }, retry_number={max_retry}")
-                max_retry += 1
-        except Exception as e:
-            logging.info(f"Trying to ping {ES_ENDPOINT}, retry_number={max_retry}, error={str(e)}")
-            max_retry += 1
-        time.sleep(2)
 
-    #send_data_to_kafka()
+if __name__ == "__main__":
+    # check our fast API is running
+    check_health(HEALTH_ENDPOINT)
+
+    # check elastic search is good to
+    check_health(ES_ENDPOINT)
+
+    # send_data_to_kafka()
     while True:
         my_schedule.run_pending()
         time.sleep(5)  # Sleep for 5 second to prevent high CPU usage
-        #This loop checks if any tasks are scheduled to run
-        #and executes them when their time comes.
-            # try sending message to kafka (producer)
-    
+        # This loop checks if any tasks are scheduled to run
+        # and executes them when their time comes.
+        # try sending message to kafka (producer)
